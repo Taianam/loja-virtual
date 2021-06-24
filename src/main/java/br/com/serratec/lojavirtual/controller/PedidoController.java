8e1ad3feaf5bea40fdca1fe0588bd0add7c99846
Alice Periles <odahcamperiles@gmail.com>
2021-06-24 19:15:04 -0300

    get por categoria produto

--- src/main/java/br/com/serratec/lojavirtual/controller/PedidoController.java
+++ src/main/java/br/com/serratec/lojavirtual/controller/PedidoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,9 +22,11 @@ import br.com.serratec.lojavirtual.service.PedidoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin(origins = "*")
 @Api
 @RestController
 @RequestMapping("/api/pedidos")
+
 public class PedidoController {
     
     @Autowired
